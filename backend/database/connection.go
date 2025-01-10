9fb20e2aaf68197638ca6b79dc9e75e0ec9fc014
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-01-10 20:00:51 +0800

    added function to create schema in sql if it does not exist

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -13,6 +13,23 @@ import (
 
 var DB *gorm.DB
 
+func createDatabase(connection *gorm.DB, dbName string) error {
+	var exists bool
+	err := connection.Raw("SELECT COUNT(*) > 0 FROM information_schema.schemata WHERE schema_name = ?", dbName).Scan(&exists).Error
+	if err != nil {
+		return fmt.Errorf("error checking if database exists: %v", err)
+	}
+
+	if !exists {
+		err := connection.Exec(fmt.Sprintf("CREATE DATABASE %s", dbName)).Error
+		if err != nil {
+			return fmt.Errorf("error creating database: %v", err)
+		}
+	}
+
+	return nil
+}
+
 func Connect() {
 	// Load .env file
 	err := godotenv.Load()
@@ -27,10 +44,23 @@ func Connect() {
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/?parseTime=true", dbUser, dbPassword, dbHost, dbPort)
 
 	// Important to parseTime=true else will have error accessing posts
 	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to mysql")
+	}
+
+	err = createDatabase(connection, dbName)
+	if err != nil {
+		panic("error creating database")
+	}
+
+	dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	connection, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("could not connect to the db")
