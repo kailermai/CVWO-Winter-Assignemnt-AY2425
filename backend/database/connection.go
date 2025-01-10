b22d92c3687976a3634abd41239d67863e6e6af5
Kai Ler <133531148+kailermai@users.noreply.github.com>
2025-01-10 18:38:01 +0800

    added .env file support

--- backend/database/connection.go
+++ backend/database/connection.go
@@ -1,8 +1,12 @@
 package database
 
 import (
+	"fmt"
+	"log"
 	"main/models"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,8 +14,23 @@ import (
 var DB *gorm.DB
 
 func Connect() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Retrieve database credentials from the environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbName)
+
 	// Important to parseTime=true else will have error accessing posts
-	connection, err := gorm.Open(mysql.Open("root:rootroot@/cvwo?parseTime=true"), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("could not connect to the db")
